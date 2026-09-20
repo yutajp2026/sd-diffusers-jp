08ce3043089b04c9a5dbc7993d137a0060b5291a
yuta <y.takak1234@gmail.com>
2026-09-20 21:18:55 +0900

    changed

--- sdwebui.sh
+++ sdwebui.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Pythonをインストールしています..."
 sudo apt update
 sudo apt install -y python3 python3-pip python3-venv
 
 if [ ! -d ".venv" ]; then
+    echo "仮想環境を作成しています..."
     python3 -m venv .venv
 fi
 
 source .venv/bin/activate
+echo "pipを更新しています..."
 python -m pip install --upgrade pip
 
 mkdir -p ~/sd_cache
 export TMPDIR=~/sd_cache
 
-pip install gradio diffusers transformers translate
-pip install torch==2.12.0 torchvision==0.27.0 --index-url https://download.pytorch.org/whl/cu126
+echo "パッケージをインストールしています..."
+pip install -r requirements.txt
 
+echo "WebUIを起動しています..."
 python main.py
